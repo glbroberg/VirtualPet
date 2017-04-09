64e5bfe2375d4ebe8e1db5c460a77f5d4e13ecf5
Garrett4 Broberg <Garrett4 Broberg>
2017-04-08 23:28:56 -0400

    Saturday night upload - pre death and full escapes

--- VirtualPet_Template/Program.cs
+++ VirtualPet_Template/Program.cs
@@ -11,13 +11,14 @@ namespace VirtualPet_Template
         static void Main(string[] args)
         {
 
+            // Introduction and Pet Naming
             VirtualPet myPet = new VirtualPet();
-            Console.Write("Hello!  Welcome to VirtuaPet! \n\n Would you like to name your pet?(y/n): ");
+            Console.Write("Hello!  Welcome to VirtuaPet! \n\nWould you like to name your pet?(y/n): ");
             string nameQuest = Console.ReadLine().ToLower();
 
             if (nameQuest == "y" || nameQuest=="yes")
             {
-                myPet.petName();
+                myPet.PetName();
             }
             else if(nameQuest=="n" || nameQuest == "no")
             {
@@ -30,68 +31,95 @@ namespace VirtualPet_Template
 
             }
 
-           
 
-
-            //TODO add a greeting for the user. you can also let the user name the pet if you like
-
+            // Main Menu
             int selectedOption;
-
             do
             {
-                //TODO fill this out with more options to interact with the pet
-                Console.WriteLine();
-                Console.WriteLine("Please select an option");
-                Console.WriteLine("1. Feed the pet");
-                Console.WriteLine("2. Put the pet to bed");
-                Console.WriteLine("3. Play with the pet");
-                Console.WriteLine("4. Give the pet a drink");
-                Console.WriteLine("5. Discipline the pet");
-                Console.WriteLine("6. Rename Pet");
-                Console.WriteLine("10. Quit");
-
-
-                selectedOption = int.Parse(Console.ReadLine());
 
                 myPet.MyPetStatus();
 
+                Console.WriteLine();
+                Console.WriteLine("Please select an option");
+                Console.WriteLine("1. Feed your pet");
+                Console.WriteLine("2. Give your pet a drink");
+                Console.WriteLine("3. Put your pet to bed");
+                Console.WriteLine("4. Play with your pet");
+                Console.WriteLine("5. Give your pet a bath");
+                Console.WriteLine("6. Discipline your pet");
+                Console.WriteLine("7. Rename Pet");
+                Console.WriteLine("8. Have pet do a magic trick");
+                //Console.WriteLine("9. Advanced Options");
+                Console.WriteLine("10. Quit");
+
+
+                selectedOption = int.Parse(Console.ReadLine());              
+
                 switch (selectedOption)
                 {
-                    //when the user selects option one we feed the pet
+                    // Feed Pet
                     case 1:
 
-                        myPet.HungerDecrease();
-                        Console.WriteLine("Thank you for feeding the pet");
+                        myPet.StomachIncrease();
+                        myPet.HitPointsIncrease();
+                        myPet.ActivityDecrease();
+                        Console.WriteLine("Yummy!! (Is what I am thinking because I can't speak!)");
                         break;
 
+                    // Hydrate Pet
                     case 2:
 
-                        myPet.HungerDecrease();
-                        Console.WriteLine("Thank you for feeding the pet");
+                        myPet.HydrationIncrease();
+                        myPet.ActivityDecrease();
+                        Console.WriteLine("Ahhh");
                         break;
 
+                    // Pet Sleep
                     case 3:
 
-                        myPet.HungerDecrease();
-                        Console.WriteLine("Thank you for feeding the pet");
+                        myPet.BehaviorIncrease();
+                        myPet.HitPointsIncrease();                          
+                        myPet.ManaIncrease();
+                        myPet.ActivityDecrease();
+                        Console.WriteLine("I feel much better after a nap, is what I would say if I could talk!");
                         break;
 
+                    // Play with Pet
                     case 4:
 
-                        myPet.HungerDecrease();
-                        Console.WriteLine("Thank you for feeding the pet");
+                        myPet.ActivityIncrease();
+                        myPet.ActivityIncrease();
+                        myPet.StomachDecrease();
+                        myPet.HitPointsDecrease();
+                        myPet.HydrationDecrease();
+                        Console.WriteLine("That was fun!! (If I could talk)");
                         break;
 
+                    // Bath Pet
                     case 5:
 
-                        myPet.HungerDecrease();
-                        Console.WriteLine("Thank you for feeding the pet");
+                        myPet.StomachDecrease();
+                        Console.WriteLine("I hate baths!");
                         break;
 
+                    // Discipline Pet
                     case 6:
 
-                        myPet.petName();
+                        myPet.Punish();
+                        Console.WriteLine("I learned my lesson");
                         break;
+                    
+                    // Rename Pet
+                    case 7:
+
+                        myPet.PetName();
+                        break;
+
+                    // Pet Magic Trick
+                    //case 8:
+
+                    //    myPet.ManaTrick();
+                    //    break;
 
                     case 10:
 
@@ -104,9 +132,8 @@ namespace VirtualPet_Template
                         break;
                 }
 
-                //TODO We can put method calls here so the pet can have some values change automatically
-                //Feel free to add, remove, or modify which methods are called here
-                myPet.HungerIncrease();
+                
+                myPet.StomachIncrease();
 
             } while (selectedOption != 10);
 
--- VirtualPet_Template/VirtualPet.cs
+++ VirtualPet_Template/VirtualPet.cs
@@ -9,68 +9,190 @@ namespace VirtualPet_Template
 
     class VirtualPet
     {
-        //Initiat Fields
+        // Initiat Fields
         private string name;
-        private int hunger;
+        private int stomach;
+        private int hydration;
         private int behavior;
         private int hitPoints;
-        private int boredem;
+        private int activity;
+        private int mana;
 
-        //this defaut constructor is sets options without giving us any options
+        // Default Constructor
         public VirtualPet()
         {
             this.name = "Nameless Loser";
-            this.hunger = 20;
+            this.stomach = 90;
+            this.hydration = 20;
             this.behavior = 20;
             this.hitPoints = 20;
-            this.boredem = 20;
-
-            //TODO account for more fields
+            this.activity = 20;
+            this.mana = 20;
         }
+
+
+        //// Name Choice Constructor
+        //public VirtualPet(string name)
+        //{
+        //    this.name = name;
+        //    this.stomach = 90;
+        //    this.hydration = 50;
+        //    this.behavior = 50;
+        //    this.hitPoints = 50;
+        //    this.activity = 50;  
+        //}
+
 
         // Assigns Pet Name
-        public void petName()
+        public void PetName()
         {
 
-            Console.WriteLine("Please type your pet's name: ");
+            Console.Write("Please type your pet's name: ");
             name = Console.ReadLine();
-        }
-
-        //TODO how can we set things with our construtor
-        public VirtualPet(string name)
-        {
-            this.name = name;
-            this.hunger = 10;
-            this.behavior = 10;
-            this.hitPoints = 10;
-            this.boredem = 10;
             
         }
 
-        
-        //this method can be called to increase the hunger
-        public void HungerIncrease()
+
+        //************* Attribute Modifying Methods **********************************  
+
+        string stomachHi = "No thanks, I'm already full!";
+        string stomachLo = "Your pet has died from hunger";
+        //Stomach Block
+        public void StomachIncrease()
         {
-            //TODO modify these values to anything you see fit
-            this.hunger = hunger + 3;
+            attribChecker(stomach,stomachHi, stomachLo);
+            this.stomach = stomach + 5;
+        }
+        public void StomachDecrease()
+        {
+            attribChecker(stomach, stomachHi, stomachLo);
+            this.stomach = stomach - 5;
         }
 
-        //this method can be called to decrease the hunger
-        public void HungerDecrease()
-        {   
-            //TODO modify these values to anything you see fit
-            this.hunger = hunger - 5;
+        // Hydration Block
+        public void HydrationIncrease()
+        {
+            this.hydration = hydration + 5;
+        }
+        public void HydrationDecrease()
+        {
+            this.hydration = hydration - 5;
         }
 
-        //TODO we need to add more methods to modify the other fields
+        // Behavior Block
+        public void BehaviorIncrease()
+        {
+            this.behavior = behavior + 5;
+        }
+        public void behaviorDecrease()
+        {
+            this.behavior = behavior - 5;
+        }
 
+        // Punishment Block
+        public void Punish()
+        {
+            Console.WriteLine("Choose how to discipline your pet: \n\t1. Stern talking-to \n\t2. Spanking");
+            string punishInput = Console.ReadLine();
+            int a;
+            bool valid = int.TryParse(punishInput, out a);
+            switch (a)
+            {
+                case 1:
+                    BehaviorIncrease();
+                    ActivityDecrease(); 
+                    break;
 
-        //method for stats of virtual pet
+                case 2:
+                    BehaviorIncrease();
+                    BehaviorIncrease();
+                    HitPointsDecrease();
+                    ManaDecrease();
+                    break;
+
+                default:
+                    Console.Write("Enter valid option number:");
+                    Punish();
+                    break;
+            }    
+        }
+
+        // Hit Points Block
+        public void HitPointsIncrease()
+        {
+            this.hitPoints = hitPoints + 5;
+        }
+        public void HitPointsDecrease()
+        {
+            this.hitPoints = hitPoints - 5;
+        }
+
+        // Bordem Block
+        public void ActivityIncrease()
+        {
+            this.activity = activity + 5;
+        }
+        public void ActivityDecrease()
+        {
+            this.activity = activity - 5;
+        }
+
+        // Mana Block
+        public void ManaIncrease()
+        {
+            this.mana = mana + 5;
+        }
+        public void ManaDecrease()
+        {
+            this.mana = mana - 5;
+        }
+        
+        //****************** End of Attribute Modifying Methods *****************************
+
+        
+        // Status Meter Conversion
+        public string StatusMeter(int a)
+        {
+            char[] level = { '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' };
+            int count = 0;
+            while (a > 0)
+            {
+                level[count] = '+';
+                count++;
+                a = a - 10; 
+            }
+            string reading = new string(level);
+            return reading;
+        }
+
+        
+        // Amount Check
+        public void attribChecker(int a, string hi, string lo)
+        {
+            if (a >= 100)
+            {
+                Console.WriteLine(hi);
+                return;
+            }
+            if (a <= 0)
+            {
+                Console.WriteLine(lo);
+                return;
+            }
+         
+        }
+
+ 
+        // Method for stats of virtual pet
         public void MyPetStatus()
         {
             Console.WriteLine(name);
-            Console.WriteLine("Hunger: " + hunger);
-            //TODO account for more fields
+            Console.WriteLine("Stomach:     " + StatusMeter(stomach)+ " "+stomach+"%");
+            Console.WriteLine("Hydration:   " + StatusMeter(hydration)+ " " + hydration + "%");
+            Console.WriteLine("Behavior:    " + StatusMeter(behavior) +" " + behavior + "%");
+            Console.WriteLine("Hit Points:  " + StatusMeter(hitPoints)+ " " + hitPoints + "%");
+            Console.WriteLine("Activity:    " + StatusMeter(activity) +" " + activity + "%");
+            Console.WriteLine("Mana:        " + StatusMeter(mana) + " " + mana + "%");
         }
     }
 }
